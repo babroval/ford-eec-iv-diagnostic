a9f3701c158f0e92b3d8dcf29b4fdf50e8646cba
babroval <maksimbabrou@gmail.com>
2018-09-30 22:35:53 +0200

    [TASK] Add an event handler for the button "ERRORS"

--- eec-iv/src/babroval/eec_iv/controller/StartController.java
+++ eec-iv/src/babroval/eec_iv/controller/StartController.java
@@ -2,12 +2,20 @@ package babroval.eec_iv.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
 import babroval.eec_iv.view.StartView;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
 
-public class StartController {
+public class StartController extends Thread {
+
+	static SerialPort serialPort = new SerialPort("COM7");;
 
 	private StartView view = new StartView();
 
@@ -21,6 +29,20 @@ public class StartController {
 			public void actionPerformed(ActionEvent ae) {
 				try {
 
+					serialPort.openPort();
+
+					serialPort.setParams(SerialPort.BAUDRATE_38400,
+										 SerialPort.DATABITS_8,
+										 SerialPort.STOPBITS_2,
+										 SerialPort.PARITY_NONE);
+
+					serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+
+					Thread.sleep(1000);
+					serialPort.writeByte((byte) 5);
+					Thread.sleep(500);
+					serialPort.writeByte((byte) 1);
+
 					JOptionPane.showMessageDialog(view.getPanel(), "The connection has been successfully established",
 							"Message", JOptionPane.INFORMATION_MESSAGE);
 
@@ -30,5 +52,20 @@ public class StartController {
 				}
 			}
 		});
+	}
+
+	private static class PortReader implements SerialPortEventListener {
+
+		@Override
+		public void serialEvent(SerialPortEvent event) {
+			if (event.isRXCHAR() && event.getEventValue() > 0) {
+				try {
+					String receivedData = serialPort.readHexString(event.getEventValue());
+					System.out.println("Received response: " + receivedData);
+				} catch (SerialPortException ex) {
+					System.out.println("Error in receiving string from COM-port: " + ex);
+				}
+			}
+		}
 	}
 }
